a057e93fe9997fc6828c5b7af028807b7cd7249c
ShashankSanil <sanilshashank9@gmail.com>
2022-05-27 15:10:52 +0530

    middleware updation

--- main.go
+++ main.go
@@ -9,6 +9,23 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	fmt.Println("Starting the application...")
 	err := godotenv.Load(".env")
@@ -25,6 +42,8 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
+
+	router.Use(CORSMiddleware())
 
 	routes.AuthRoutes(router)
 	routes.UserRoutes(router)
